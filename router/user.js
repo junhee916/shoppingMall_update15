65ce049389f873b1e21aa6457d9854d538080cd1
junhee916 <junhee916@naver.com>
2021-04-23 16:36:37 +0900

    double checked userInfo

--- router/user.js
+++ router/user.js
@@ -4,43 +4,82 @@ const bcrypt = require('bcryptjs')
 const router = express.Router()
 
 // userInfo
+router.get("/", (req, res) => {
+
+    userModel
+        .find()
+        .then(users => {
+            res.json({
+                msg : "get users",
+                count : users.length,
+                userInfo : users.map(user => {
+                    return{
+                        id : user._id,
+                        name : user.name,
+                        email : user.email,
+                        data : user.createdAt
+                    }
+                })
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                msg : err.message
+            })
+        })
+})
 
 // 회원가입
 router.post("/signup", (req, res) => {
 
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
+    userModel
+        .findOne({email : req.body.userEmail})
+        .then(user => {
+            if(user){
+                return res.json({
+                    msg : "user exited, please other email"
+                })
+            }
+            else{
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
 
-        if(err){
-            return res.status(500).json({
+                    if(err){
+                        return res.status(500).json({
+                            msg : err.message
+                        })
+                    }
+                    else{
+                        const newUser = new userModel(
+                            {
+                                name : req.body.userName,
+                                email : req.body.userEmail,
+                                password : hash
+                            }
+                        )
+
+                        newUser
+                            .save()
+                            .then(user => {
+                                res.json({
+                                    msg : "register user",
+                                    userInfo : user
+                                })
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    msg : err.message
+                                })
+                            })
+                    }
+
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
                 msg : err.message
             })
-        }
-        else{
-            const newUser = new userModel(
-                {
-                    name : req.body.userName,
-                    email : req.body.userEmail,
-                    password : hash
-                }
-            )
-
-            newUser
-                .save()
-                .then(user => {
-                    res.json({
-                        msg : "register user",
-                        userInfo : user
-                    })
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        msg : err.message
-                    })
-                })
-        }
-
-    })
-
+        })
 
 })
 
